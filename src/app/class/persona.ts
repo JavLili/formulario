50afef5ec4e55737bab5976af1e907ad13416343
JavLili <hector.vargas@fastmoda.com.co>
2023-01-25 16:58:50 -0500

    Se inicia el front con los 21 inputs

--- src/app/class/persona.ts
+++ src/app/class/persona.ts
@@ -1,8 +1,23 @@
 export class Persona {
-    id:number;
-    cc:number;
-    nombre:string;
-    celular: number;    
+    identificacion:number;
+    nombres:string;
+    apellidos:string;
+    poblacion:string;
+    region:string;
     direccion:string;
-    correo:string;
+    telefono:number;
+    celular: number;    
+    email:string;
+    sexo:string;
+    fecha_nacimiento:string;
+    organizacion_ventas:string;
+    canal_distribucion:string;
+    sector:string;
+    grupo_clientes:string;
+    zona_ventas:string;
+    grupo_vendedores:string;
+    oficina_ventas:string;
+    grupo_cliente1:string;
+    grupo_cliente3:string;
+    cupo:string;
 }
