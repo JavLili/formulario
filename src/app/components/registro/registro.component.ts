b88f12b1eb6233b31a5ab641dc4dc45ccfe92f25
JavLili <hector.vargas@fastmoda.com.co>
2023-01-27 14:32:05 -0500

    Formulario con validaciones reactivas basicas

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from '../../class/persona';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { PersonaService } from '../../services/persona.service';
 
 @Component({
@@ -9,8 +9,9 @@ import { PersonaService } from '../../services/persona.service';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit{
-
-  form: FormGroup
+  
+  submitted = false;
+  register! : FormGroup;  
 
   nombre1:string="";
   nombre2:string="";
@@ -18,40 +19,56 @@ export class RegistroComponent implements OnInit{
   apellido2: string="";
   fullName: string="";
   persona : Persona = new Persona();
-  constructor(private personaService : PersonaService, private fb: FormBuilder){
-    this.form 
+  constructor(private personaService : PersonaService, public fb: FormBuilder){}
+  ngOnInit():void {
+    this.register = this.fb.group({
+      identificacion:['', Validators.required],
+      nombre1 :['', Validators.required],
+      nombre2 :['', Validators.nullValidator],
+      apellido1 :['', Validators.required],
+      apellido2 :['', Validators.required],
+      poblacion:['', Validators.required],
+      region:['', Validators.required],
+      direccion:['', Validators.required],
+      telefono:['', Validators.required],
+      celular:['', Validators.required],
+      inlineRadioOptions:['', Validators.required],
+      email:['', Validators.required],
+      fecha_nacimiento:['', Validators.required],
+      organizacion_ventas:['', Validators.required],
+      canal_distribucion:['', Validators.required],
+      sector:['', Validators.required],
+      grupo_clientes:['', Validators.required],
+      zona_ventas:['', Validators.required],
+      grupo_vendedores:['', Validators.required],
+      oficina_ventas:['', Validators.required],
+      grupo_clientes1:['', Validators.required],
+      grupo_clientes3:['', Validators.required],
+      cupo:['', Validators.required],
+    })
   }
-  ngOnInit(): void {
-    this.form = this.initForm();
+
+  onSubmit(){
+    this.submitted=true
+    
+    if (this.register.invalid) {
+      console.log(this.register.value);
+      return
+    }
+    alert("Success")
   }
 
   enviar(){
     this.personaService.registrarCliente(this.persona).subscribe(dato=>{
       console.log(dato);      
     },error => console.log(error));
-  }
-  
+    }
+    
   concatenar(){
-    if (this.nombre2==="") {
-      this.fullName=this.nombre1+" "+this.apellido1+" "+this.apellido2
-      this.persona.nombres=this.nombre1
-      this.persona.apellidos=this.apellido1+" "+this.apellido2
+    if (this.register.controls.nombre1.value==="") {
+      this.fullName=this.register.controls.nombre1.value+" "+this.register.controls.apellido1.value+" "+this.register.controls.apellido2.value
     } else{
-      this.fullName=this.nombre1+" "+this.nombre2+" "+this.apellido1+" "+this.apellido2
-      this.persona.nombres=this.nombre1+" "+this.nombre2
-      this.persona.apellidos=this.apellido1+" "+this.apellido2
+      this.fullName=this.register.controls.nombre1.value+" "+this.register.controls.nombre2.value+" "+this.register.controls.apellido1.value+" "+this.register.controls.apellido2.value
     }
   }
-  
-  initForm(): FormGroup{
-    return this.fb.group({
-      nombre1:['', [Validators.required, Validators.minLength(3)]],
-      apellido1:['', [Validators.required, Validators.minLength(4)]],
-      apellido2:['', [Validators.required, Validators.minLength(4)]],
-      cc:['', [Validators.required, Validators.minLength(4)]],
-      email:['', [Validators.required, Validators.minLength(4)]],
-      celular:['', [Validators.required, Validators.minLength(4)]],
-      direccion:['', [Validators.required, Validators.minLength(4)]]
-    })
-  }  
 }
